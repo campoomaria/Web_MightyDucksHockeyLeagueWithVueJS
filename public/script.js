e845d32e157aa625483f45c747c49130d8697081
campoomaria <campoomariadelosangeles@gmail.com>
2022-09-21 23:53:29 -0300

    continuacion de proyecto

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const { createApp } = Vue;
 createApp({
   components: {
-    template: "#modal-template"
+    template: "#modal-template",
   },
   data() {
     return {
@@ -10,9 +10,129 @@ createApp({
       mapa: `mapa1`,
       urlmap: `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.6572038332524!2d-87.63125808459743!3d41.900228779220456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd34e07f69da7%3A0x15e198c063fc787c!2sAJ%20Katzenmaier%20Elementary!5e0!3m2!1ses-419!2sar!4v1654568785582!5m2!1ses-419!2sar"`,
       page: `home`,
+ 
+      // registro de usuario
+      emailRegistro: "",
+      passwordRegistro: "",
+
+      // inicio de sesion
+      emailInicio: "",
+      passwordInicio: "",
+
+       // usuario
+       alias: 'Anonimo',
+       foto: './assets/images/avatar.png',
+       inicioSesion: false,
+       usuario: null,
+
+      eventSeptember: [
+        {
+          date: `9/01`,
+          time: `9:30 am`,
+          location: `AJKatzenmaier`,
+          team: `U1 and U4`,
+        },
+        {
+          date: `9/01`,
+          time: `1:00 pm`,
+          location: `Greenbay`,
+          team: `U3 and U2`,
+        },
+        {
+          date: `9/08`,
+          time: `9:30 am`,
+          location: `Howard A.Y`,
+          team: `U5 and U6`,
+        },
+        {
+          date: `9/08`,
+          time: `1:00 pm`,
+          location: `Marjorie P.H`,
+          team: `U6 and U1`,
+        },
+        {
+          date: `9/15`,
+          time: `9:30 am`,
+          location: `North Elementary`,
+          team: `U2 and U4`,
+        },
+        {
+          date: `9/15`,
+          time: `1:00 pm`,
+          location: `AJKatzenmaier`,
+          team: `U3 and U5`,
+        },
+        {
+          date: `9/22`,
+          time: `9:30 am`,
+          location: `South Elementary`,
+          team: `U1 and U3`,
+        },
+        {
+          date: `9/22`,
+          time: `1:00 pm`,
+          location: `Howard A.Y.`,
+          team: `U2 and U6`,
+        },
+        {
+          date: `9/29`,
+          time: `9:30 am`,
+          location: `Greenbay`,
+          team: `U4 and U5`,
+        },
+      ],
+      eventOctober: [
+        {
+          date: `10/06`,
+          time: `9:30 am`,
+          location: `Marjorie P.H.`,
+          team: `U2 and U5`,
+        },
+        {
+          date: `10/06`,
+          time: `1:00 pm`,
+          location: `South Elementary`,
+          team: `U1 and U6`,
+        },
+        {
+          date: `10/08`,
+          time: `9:30 am`,
+          location: `Howard A.Y.`,
+          team: `U3 and U4`,
+        },
+        {
+          date: `10/08`,
+          time: `1:00 pm`,
+          location: `Greenbay`,
+          team: `U5 and U1`,
+        },
+        {
+          date: `10/20`,
+          time: `9:30 am`,
+          location: `North Elementary`,
+          team: `U6 and U3`,
+        },
+        {
+          date: `10/20`,
+          time: `1:00 pm`,
+          location: `Marjorie P.H.`,
+          team: `U2 and U4`,
+        },
+        {
+          date: `10/27`,
+          time: `9:30 am`,
+          location: `AJKatzenmaier`,
+          team: `U3 and U1`,
+        },
+        {
+          date: `10/27`,
+          time: `1:00 pm`,
+          location: `Howard A.Y.`,
+          team: `U5 and U6`,
+        },
+      ],
     };
   },
-  created() {},
   methods: {
     showModal: function (element) {
       // limpio los links (sacando la clase on)
@@ -50,5 +170,54 @@ createApp({
         this.urlmap = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3052.117187982298!2d-74.70157078441592!3d40.095100083060935!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c144b2638ff551%3A0x7d8ba7c4c7e2bf98!2sS%20Hockey%20Dr%2C%20Mansfield%2C%20NJ%2008022%2C%20EE.%20UU.!5e0!3m2!1ses!2sar!4v1651157948661!5m2!1ses!2sar`;
       }
     },
+    crearUsuario() {
+      if (this.emailRegistro != "" && this.passwordRegistro != "") {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(
+            this.emailRegistro,
+            this.passwordRegistro
+          )
+          .then((userCredential) => {
+            var user = userCredential.user;
+            this.emailRegistro = "";
+            this.passwordRegistro = "";
+          })
+          .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+          })
+      }
+    },
+    iniciarSesion() {
+      if(this.emailInicio != '' && this.passwordInicio != ''){
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.emailInicio, this.passwordInicio)
+        .then((userCredential) => {
+          var user = userCredential.user;
+          this.usuario = user
+          this.alias = this.usuario.email
+          this.iniciarSesion = true
+          this.page='home'
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+        })
+      }
+    },
+    cerrarSesion() {
+      firebase.auth().signOut();
+      
+      this.usuario = null
+      this.alias = 'Anonimo'
+      this.pagina = 'Inicio'
+      this.inicioSesion = false
+
+      this.emailInicio = ''
+      this.passwordInicio = ''
+      document.getElementById('avatar').src = this.foto
+  },
   },
 }).mount("#app");
